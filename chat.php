1aeacc03aba83a58adcc98270f63675059929e8e
ornab <hzamil17@gmail.com>
2016-12-24 01:16:17 +0600

    initial commit

---
+++ chat.php
@@ -0,0 +1,13 @@
+<?php 
+	include 'db.php';
+	
+	$query = "SELECT * FROM chat ORDER BY id DESC";
+	$run = $con->query($query);
+	while($row = $run->fetch_array()) :
+		?>
+			<div id="chat_data">
+				<span style="color:green;"><?php echo $row['name']; ?></span> :
+				<span style="color:brown;"><?php echo $row['msg']; ?></span>
+				<span style="float:right;"><?php echo formatDate($row['date']); ?></span>
+			</div>
+			<?php endwhile;?>
